1a6e0ef5506a769bb67270d27a6aaa5b8d1401a4
Subodhini <subodhiniaggarwal@gmail.com>
2024-12-03 23:14:38 +0530

    ;

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
 
 function App() {
 
   return (
     <>
      <h1>Learning Redux</h1>
+     <AddTodo/>
+     <Todos/>
     </>
   )
 }
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {addTodo} from '../features/todoSlice';
+import {addTodo} from '../features/todo/todoSlice';
 
 function AddTodo() {
     const [input,setInput]=useState("");
